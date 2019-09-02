b5c6482d38d647fd8348b951d39e76f98b09714f
José Pinto <jose.ferropinto@epfl.ch>
2019-09-02 15:46:24 +0200

    UPDATE : index connects to API and gets status and user

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,50 @@ const express = require('express');
 const router = express.Router();
 const Habit = require('../lib/habitica');
 
-const currentHabit = new Habit("APIUser", "APItoken");
+//* API officiel
+let userId = "APIUser";
+let apiKey = "APItoken";
+// */
+
+/* API vide
+let userId = undefined;
+let apiKey = undefined;
+// */
+
+
+const currentHabit = new Habit(userId, apiKey);
+let user = undefined;
 
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
     currentHabit.getStatus(function (error, response) {
-        res.render('index', {
-            title: 'Express',
-            apiStatus: JSON.parse(response.text).data.status
-        });
+        let vue = 'index';
+        let apiStatus = JSON.parse(response.text).data.status;
+        const ready = userId !== undefined && apiKey !== undefined;
+        if (!ready) {
+            res.render(vue, {
+                logged: ready,
+                apiStatus: apiStatus
+            })
+        } else {
+            if (user === undefined) {
+                currentHabit.getUser(function (error, response) {
+                    user = JSON.parse(response.text).data;
+                    res.render(vue, {
+                        logged: ready,
+                        apiStatus: apiStatus,
+                        profile: user.auth.local.username
+                    });
+                });
+            } else {
+                res.render(vue, {
+                    logged: ready,
+                    apiStatus: apiStatus,
+                    profile: user.auth.local.username
+                });
+            }
+        }
     });
 });
 module.exports = router;
