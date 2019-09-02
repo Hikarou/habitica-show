22197c431a1635de613ff7c07e442c2dc7959679
José Pinto <jose.ferropinto@epfl.ch>
2019-09-02 11:51:27 +0200

    UPDATE : index shows API status now

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const router = express.Router();
+const Habit = require('../lib/habitica');
+
+const currentHabit = new Habit("APIUser", "APItoken");
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    currentHabit.getStatus(function (error, response) {
+        res.render('index', {
+            title: 'Express',
+            apiStatus: JSON.parse(response.text).data.status
+        });
+    });
 });
-
 module.exports = router;
