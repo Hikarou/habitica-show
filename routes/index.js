ed0c13398c9fcdecd1b11ed37a36246ee8db07e0
José Pinto <jose.ferropinto@epfl.ch>
2019-09-04 10:55:05 +0200

    REFACTOR : Comments and login gestion

--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,46 @@ const express = require('express');
 const router = express.Router();
 const Habit = require('../lib/habitica');
 
-
+// Current params for API connection
 let userId = "0";
 let apiKey = "0";
 let apiUrl = undefined;
 let user = undefined;
 let currentHabit = new Habit(userId, apiKey);
 
-/* GET home page. */
+/**
+ * Get all the tasks for render in index
+ * @param res The response that render will be call upon
+ * @param vue The view to be called
+ * @param ready Either there is someone logged
+ * @param apiStatus The apiStatus
+ */
+const showTasks = function (res, vue, ready, apiStatus) {
+    currentHabit.getTodoTasks(function (error, response) {
+        let todos = response.body.data;
+        currentHabit.getDailiesTasks(function (error, response) {
+            let dailies = response.body.data;
+            currentHabit.getHabitsTasks(function (error, response) {
+                let habits = response.body.data;
+                res.render(vue, {
+                    logged: ready,
+                    apiStatus: apiStatus,
+                    profile: user.auth.local.username,
+                    todos: todos,
+                    dailies: dailies,
+                    habits: habits
+                });
+            });
+        });
+    });
+};
+
+// Get the Home page
 router.get('/', function (req, res, next) {
-    // get the status of the official API
+    // get the status of the API or the official API if apiUrl not defined
     currentHabit.getStatus(function (error, response) {
-        let vue = 'index';
-        let apiStatus = JSON.parse(response.text).data.status;
+        const vue = 'index';
+        const apiStatus = JSON.parse(response.text).data.status;
         const ready = userId !== "0" && apiKey !== "0";
         // If the user is not logged
         if (!ready) {
@@ -26,38 +53,18 @@ router.get('/', function (req, res, next) {
             // Logging
             if (user === undefined) {
                 currentHabit.getUser(function (error, response) {
-                    let user = JSON.parse(response.text).data;
-                    currentHabit.getTodoTasks(function (error, response) {
-                        let todos = response.body.data;
-                        currentHabit.getDailiesTasks(function (error, response) {
-                            let dailies = response.body.data;
-                            currentHabit.getHabitsTasks(function (error, response) {
-                                let habits = response.body.data;
-                                res.render(vue, {
-                                    logged: ready,
-                                    apiStatus: apiStatus,
-                                    profile: user.auth.local.username,
-                                    todos: todos,
-                                    dailies: dailies,
-                                    habits: habits
-                                });
-                            });
-
-                        });
-                    });
+                    user = JSON.parse(response.text).data;
+                    showTasks(res, vue, ready, apiStatus);
                 });
             } else { // Logged
-                res.render(vue, {
-                    logged: ready,
-                    apiStatus: apiStatus,
-                    profile: user.auth.local.username
-                });
+                showTasks(res, vue, ready, apiStatus);
             }
         }
     });
 });
 
-router.post('/', function (req, res, next) {
+// For log in purpose
+router.post('/login', function (req, res, next) {
     userId = req.body.userId;
     apiKey = req.body.apiKey;
     apiUrl = req.body.apiUrl;
@@ -65,6 +72,7 @@ router.post('/', function (req, res, next) {
     res.redirect('/');
 });
 
+// For logout purpose
 router.post('/logout', function (req, res, next) {
     userId = "0";
     apiKey = "0";
