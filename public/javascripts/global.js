2cff6b7f71bc447cae31f83aea147ecb66459b52
José Pinto <jose.ferropinto@epfl.ch>
2019-09-06 15:18:46 +0200

    UPDATE : better usage of findById ADD: new navigator CLEANUP : index.js with old function from server-side ADD: Loading animation for tasks

--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -1,26 +1,37 @@
 let connection = undefined;
 let user = undefined;
-let todos = [];
-let dailies = [];
-let habits = [];
+let todos = undefined;
+let dailies = undefined;
+let habits = undefined;
+
+const inputUserID = $('#userId');
+const inputApiKey = $('#apiKey');
+const inputApiUrl = $('#apiUrl');
+const pageLogin = $('#login');
+const pageLogged = $('#logged');
+const catTodos = $('#todos');
+const catDailies = $('#dailies');
+const catHabits = $('#habits');
+const welcomeMessage = $('#welcome');
+const banniereLogin = $('#h_login');
+const banniereLogged = $('#h_logged');
 
 $(document).ready(function () {
     $('#btn-login').on('click', login);
     $('#btn-logout').on('click', logout);
+    banniereLogged.hide();
 });
 
-function login(event) {
-    let userID = $('#userId').val();
-    let apiKey = $('#apiKey').val();
-    let apiUrl = $('#apiUrl').val();
-    if (undefined !== userID && undefined !== apiKey) {
+function login() {
+    let valUserID = inputUserID.val();
+    let valApiKey = inputApiKey.val();
+    let valApiUrl = inputApiUrl.val();
+    if (undefined !== valUserID && undefined !== valApiKey) {
         connection = {
-            userId: userID,
-            apiKey: apiKey,
-            apiUrl: apiUrl
+            userId: valUserID,
+            apiKey: valApiKey,
+            apiUrl: valApiUrl
         };
-
-        console.log("connection :" + JSON.stringify(connection));
 
         $.getJSON('/getUser', connection, function (data, status) {
             if (status === "success") {
@@ -39,22 +50,22 @@ function login(event) {
     }
 }
 
-function logout(event) {
+function logout() {
     connection = undefined;
     user = undefined;
-    todos = [];
-    dailies = [];
-    habits = [];
-    $('#login').show();
-    $('#logged').hide();
+    todos = undefined;
+    dailies = undefined;
+    habits = undefined;
+    pageLogin.show();
+    pageLogged.hide();
 }
 
 /**
  * Get all the tasks for render in index
- * @param force If force refresh
  */
 const showTasks = function () {
-    $('#login').hide();
+    pageLogin.hide();
+    banniereLogin.hide();
     $.getJSON('/getTasks', connection, function (data, status) {
         console.log(status);
         if (status === "success") {
@@ -62,23 +73,23 @@ const showTasks = function () {
             dailies = data.dailies;
             habits = data.habits;
 
-            console.log(todos[0].text);
             let todosToShow = '<h1>TODOS</h1>\n';
             todos.forEach(elem => todosToShow += '<div class="task">' + elem.text + '</div>\n');
-            $('#todos').html(todosToShow);
+            catTodos.html(todosToShow);
 
             let dailiesToShow = '<h1>DAILIES</h1>\n';
             dailies.forEach(elem => dailiesToShow += '<div class="task">' + elem.text + '</div>\n');
-            $('#dailies').html(dailiesToShow);
+            catDailies.html(dailiesToShow);
 
             let haibitsToShow = '<h1>HABITS</h1>\n';
             habits.forEach(elem => haibitsToShow += '<div class="task">' + elem.text + '</div>\n');
-            $('#habits').html(haibitsToShow);
+            catHabits.html(haibitsToShow);
         } else {
             alert('Something went wrong with retrieving the tasks');
             return false;
         }
     });
-    $('#welcome').html('Welcome ' + user.auth.local.username + ' !');
-    $('#logged').show();
+    welcomeMessage.html('Welcome ' + user.auth.local.username + ' !');
+    pageLogged.show();
+    banniereLogged.show();
 };
--- routes/index.js
+++ routes/index.js
@@ -2,79 +2,18 @@ const express = require('express');
 const router = express.Router();
 const Habit = require('../lib/habitica');
 
-// Current params for API connection
-let userId = "0";
-let apiKey = "0";
-let apiUrl = undefined;
-let user = undefined;
-let currentHabit = new Habit(userId, apiKey);
-
 /**
- * Get all the tasks for render in index
- * @param res The response that render will be call upon
- * @param vue The view to be called
- * @param ready Either there is someone logged
- * @param apiStatus The apiStatus
+ * Get the homepage route
  */
-const showTasks = function (res, vue, ready, apiStatus) {
-    currentHabit.getTodoTasks(function (error, response) {
-        let todos = error !== undefined ? response.body.data : [];
-        currentHabit.getDailiesTasks(function (error, response) {
-            let dailies = error !== undefined ? response.body.data : [];
-            currentHabit.getHabitsTasks(function (error, response) {
-                let habits = error !== undefined ? response.body.data : [];
-                res.render(vue, {
-                    logged: ready,
-                    apiStatus: apiStatus,
-                    profile: user.auth.local.username,
-                    todos: todos,
-                    dailies: dailies,
-                    habits: habits
-                });
-            });
-        });
-    });
-};
-
-// Get the Home page
 router.get('/', function (req, res, next) {
     res.render('index');
-    /*
-    // get the status of the API or the official API if apiUrl not defined
-    currentHabit.getStatus(function (error, response) {
-        if (response !== undefined && response.ok) {
-            const vue = 'index';
-            const apiStatus = JSON.parse(response.text).data.status;
-            const ready = userId !== "0" && apiKey !== "0";
-            // If the user is not logged
-            if (!ready) {
-                res.render(vue, {
-                    logged: ready,
-                    apiStatus: apiStatus
-                })
-            } else {
-                // Logging
-                if (user === undefined) {
-                    currentHabit.getUser(function (error, response) {
-                        user = JSON.parse(response.text).data;
-                        showTasks(res, vue, ready, apiStatus);
-                    });
-                } else { // Logged
-                    showTasks(res, vue, ready, apiStatus);
-                }
-            }
-        } else {
-            habiticaReset();
-            res.status = 503;
-            res.render("error", {error: error, message: "API (" + apiUrl + ") is not reachable"});
-        }
-    });
-
-    // */
 });
 
+/**
+ * The the current user route
+ */
 router.get('/getUser', function (req, res, next) {
-    currentHabit = new Habit(req.query.userId, req.query.apiKey, req.query.apiUrl);
+    let currentHabit = new Habit(req.query.userId, req.query.apiKey, req.query.apiUrl);
     currentHabit.getUser(function (error, response) {
         let user = JSON.parse(response.text).data;
         res.json({
@@ -83,8 +22,11 @@ router.get('/getUser', function (req, res, next) {
     });
 });
 
+/**
+ * Get the tasks of the current user route
+ */
 router.get('/getTasks', function (req, res, next) {
-    currentHabit = new Habit(req.query.userId, req.query.apiKey, req.query.apiUrl);
+    let currentHabit = new Habit(req.query.userId, req.query.apiKey, req.query.apiUrl);
     currentHabit.getTodoTasks(function (error, response) {
         let todos = error !== undefined ? response.body.data : [];
         currentHabit.getDailiesTasks(function (error, response) {
